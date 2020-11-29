fb5059cad1cfdfc1d989cecdb72351c0ce8896d9
mjs0818 <tjfaudwo09@gmail.com>
2020-11-29 16:59:10 +0900

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react"
+import AppRouter from "./Router"
+import { authService } from "../fbase"
+function App() {
+  const [init, setInit] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser)
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true)
+      } else {
+        setIsLoggedIn(false)
+      }
+      setInit(true)
+    })
+  }, [])
+  return (
+    <>
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
+      <footer>&copy; Nwitter {new Date().getFullYear()}</footer>
+    </>
+  )
+}
+
+export default App
---
+++ src/fbase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase"
+import "firebase/auth"
+import "firebase/firestore"
+const firebaseConfig = {
+  apiKey: "AIzaSyDn51bS0jBsRhiXZCmBYdxL7aj6D_pGCfY",
+  authDomain: "test-1c241.firebaseapp.com",
+  databaseURL: "https://test-1c241.firebaseio.com",
+  projectId: "test-1c241",
+  storageBucket: "test-1c241.appspot.com",
+  messagingSenderId: "628108227892",
+  appId: "1:628108227892:web:9d6f4239a8f30976d02161",
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+export const firebaseInstance = firebase
+export const authService = firebase.auth()
+export const dbService = firebase.firestore()
